9aa139dfde7f71a99ca9cd5f8c75ec07fe2800ac
임현호 <limhh0801@hyundai.com>
2024-09-24 14:48:02 +0900

    feat: 게시글이 업데이트되면 저장한다

--- src/main/kotlin/com/kotlin/rss/Main.kt
+++ src/main/kotlin/com/kotlin/rss/Main.kt
@@ -2,6 +2,7 @@ package com.kotlin.rss
 
 import com.kotlin.rss.controller.RssController
 import com.kotlin.rss.model.Item
+import com.kotlin.rss.model.ItemMemory
 import kotlinx.coroutines.*
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
@@ -10,18 +11,20 @@ import kotlin.time.Duration
 fun main() {
     runBlocking {
         val controller = RssController()
-        var latestFeeds: List<Item> = listOf()
+        val itemMemory = ItemMemory()
         launch {
             while (true) {
                 println("${Thread.currentThread().name} while문 반복중")
                 val channel = getChannel()
-                latestFeeds = channel.toItemList()
+                itemMemory.addAll(channel.toItemList())
+                val latestFeeds = itemMemory.items
                 controller.showLatestFeeds(latestFeeds)
                 delay(Duration.parse("5s"))
             }
         }
         launch(Dispatchers.IO) {
             println("${Thread.currentThread().name} 검색어 입력기다리는중")
+            val latestFeeds = itemMemory.items
             controller.searchList(latestFeeds)
         }
     }
