4d3d6ab7d597d5ea0556913280139d87f7df4b3f
INHAWOO <wih6461@gmail.com>
2024-09-24 13:35:02 +0900

    feat: 한 번 실행하면 10분마다 기술 블로그의 RSS 업데이트를 확인한다

--- src/main/kotlin/com/kotlin/rss/Main.kt
+++ src/main/kotlin/com/kotlin/rss/Main.kt
@@ -2,18 +2,32 @@ package com.kotlin.rss
 
 import com.kotlin.rss.controller.RssController
 import com.kotlin.rss.model.Item
+import kotlinx.coroutines.*
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
+import kotlin.time.Duration
 
 fun main() {
-    val factory = DocumentBuilderFactory.newInstance()
-    val xml = factory.newDocumentBuilder()
-        .parse("https://techblog.woowahan.com/feed")
-    val channel = xml.getElementsByTagName("channel").item(0)
-    val controller = RssController()
+    runBlocking {
+        val controller = RssController()
+        var latestFeeds: List<Item> = listOf()
+        launch {
+            while (true) {
+                val channel = getChannel()
+                latestFeeds = channel.toItemList()
+                controller.showLatestFeeds(latestFeeds)
+                delay(Duration.parse("10m"))
+            }
+        }
+        launch {
+            controller.searchList(latestFeeds)
+        }
+    }
+}
 
-    val latestFeeds = channel.toItemList()
-
-    controller.showLatestFeeds(latestFeeds)
-    controller.searchList(latestFeeds)
+fun getChannel(): Node {
+        val factory = DocumentBuilderFactory.newInstance()
+        val xml = factory.newDocumentBuilder()
+            .parse("https://techblog.woowahan.com/feed")
+        return xml.getElementsByTagName("channel").item(0)
 }
